c15d9f49a0fd1448fa44b22cf14da86954997344
Edwin Donovan <edwin.donovan@mail.mcgill.ca>
2025-03-21 16:51:51 -0400

    fixing environment var converter

--- kestra/convert_env_to_encoded.sh
+++
@@ -1,4 +0,0 @@
-while IFS='=' read -r key value; do
-    echo "SECRET_$key=$(echo -n "$value" | base64)";
-done < .env > .env_encoded
-echo "SECRET_GCP_SERVICE_ACCOUNT=$(cat ~/.pki/gcp/creds.json | base64 -w 0)" >> .env_encoded
