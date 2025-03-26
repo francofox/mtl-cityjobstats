fc2e9ae058e7b96fa29e4f034d7bdeb740a15c2c
Edwin Donovan <edwin.donovan@mail.mcgill.ca>
2025-03-26 18:44:10 +0000

    fixing nomenclature

--- dbt/models/core/dim_classifications.sql
+++ dbt/models/core/dim_classifications.sql
@@ -8,4 +8,4 @@ select
     classif,
     classif_desc as classif_fr,
     classif_desc_en as classif_en
-FROM {{ ref('dim_classif') }}
+FROM {{ ref('classifications') }}
--- dbt/models/core/dim_lv1units.sql
+++ dbt/models/core/dim_lv1units.sql
@@ -8,4 +8,4 @@ select
     {{ dbt.safe_cast("unit_lv1", api.Column.translate_type("integer")) }} as unit_lv1,
     {{ dbt.safe_cast("unit_lv1_desc", api.Column.translate_type("string")) }} as unit_lv1_fr,
     {{ dbt.safe_cast("unit_lv1_desc_en", api.Column.translate_type("string")) }} as unit_lv1_en
-from {{ ref('dim_lv1unit') }}
+from {{ ref('lv1units') }}
