fdda754c56b857bc47021906c55e94f06821d375
Edwin Donovan <edwin.donovan@mail.mcgill.ca>
2025-03-31 19:03:23 +0000

    forgot to include averages

--- dbt/models/core/fct_applicant_demography_per_job.sql
+++ dbt/models/core/fct_applicant_demography_per_job.sql
@@ -30,15 +30,21 @@ applicant_info as (
 select
     a.jobtitle as jobtitle,
     a.total_applicants as total_applicants,
+    a.avg_appl_per_job as avg_applicants,
     i.num_women_applicants as women_applicants,
     case when a.total_applicants <> 0 then i.num_women_applicants / a.total_applicants else 0 end as percent_women,
+    case when a.total_applicants <> 0 then (i.num_women_applicants / a.total_applicants) * a.avg_appl_per_job else 0 end as avg_num_women,
     i.num_disabled_applicants as disabled_applicants,
     case when a.total_applicants <> 0 then i.num_disabled_applicants / a.total_applicants else 0 end as percent_disabled,
+    case when a.total_applicants <> 0 then (i.num_disabled_applicants / a.total_applicants) * a.avg_appl_per_job else 0 end as avt_num_disabled,
     i.num_minority_applicants as minority_applicants,
     case when a.total_applicants <> 0 then i.num_minority_applicants / a.total_applicants else 0 end as percent_minority,
+    case when a.total_applicants <> 0 then (i.num_minority_applicants / a.total_applicants) * a.avg_appl_per_job else 0 end as avg_num_minority,
     i.num_indigenous_applicants as indigenous_applicants,
     case when a.total_applicants <> 0 then i.num_indigenous_applicants / a.total_applicants else 0 end as percent_indigenous,
+    case when a.total_applicants <> 0 then (i.num_indigenous_applicants / a.total_applicants) * a.avg_appl_per_job else 0 end as avg_num_indigenous,
     i.num_ethnicminority_applicants as ethnicminority_applicants,
-    case when a.total_applicants <> 0 then i.num_ethnicminority_applicants / a.total_applicants else 0 end as percent_ethnicminority
+    case when a.total_applicants <> 0 then i.num_ethnicminority_applicants / a.total_applicants else 0 end as percent_ethnicminority,
+    case when a.total_applicants <> 0 then (i.num_ethnicminority_applicants / a.total_applicants) * a.avg_appl_per_job else 0 end as avg_num_ethnicminority
 from num_of_applicants a 
 left join applicant_info i on i.jobcode = a.jobcode
